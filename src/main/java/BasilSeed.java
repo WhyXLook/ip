dffe1e247496acf37219a9f41d2d74e7fbfbbe9f
Edmund <l>
2025-08-21 05:53:14 +0800

    Added Echo Functionality

--- src/main/java/BasilSeed.java
+++ src/main/java/BasilSeed.java
@@ -1,11 +1,24 @@
+import java.util.Scanner;
+
 public class BasilSeed {
     public static void main(String[] args) {
         String outMsg = "____________________________________________________________\n" +
                 " Hello! I'm BasilSeed \n" +
                 " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
                 "____________________________________________________________\n";
         System.out.println(outMsg);
+        // Use of scanner to get user input came from https://stackoverflow.com/questions/5287538/how-to-get-the-user-input-in-java
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        String userInput = reader.next();
+        for(;!userInput.equals("bye"); userInput = reader.next()){
+            System.out.println("User typed: " + userInput);
+        }
+        System.out.println("""
+                    ____________________________________________________________
+                     Bye. Hope to see you again soon!
+                    ____________________________________________________________\
+                """);
+        reader.close();
     }
 }
