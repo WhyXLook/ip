6be9c51623d6de24459c3e4abad4c495ee0bcc47
Edmund <l>
2025-08-21 05:29:10 +0800

    Edited Duke to become BasilSeed

---
+++ src/main/java/BasilSeed.java
@@ -0,0 +1,11 @@
+public class BasilSeed {
+    public static void main(String[] args) {
+        String outMsg = "____________________________________________________________\n" +
+                " Hello! I'm BasilSeed \n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________\n";
+        System.out.println(outMsg);
+    }
+}
