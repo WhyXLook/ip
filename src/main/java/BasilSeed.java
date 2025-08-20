503d7fb4a7cbdfbad3e7bbee941aaaf917018564
Edmund <l>
2025-08-21 06:42:59 +0800

    Added the add and list feature. Also edited so that it reads entire user input instead of word by word

--- src/main/java/BasilSeed.java
+++ src/main/java/BasilSeed.java
@@ -1,18 +1,50 @@
 import java.util.Scanner;
 
 public class BasilSeed {
+    // String[] userInputs = new String[100];
+
+
+    public static void commandParser (String inputString, String[] inputArray){
+        /*
+        Function to parse user input, checking if its a command keyword. i.e. List
+        Modify the passed in array as needed by the command.
+        A null variable marks the end of the array while a empty string marks a deleted one.
+        */;
+        switch (inputString){
+            case "list":
+                for (String s : inputArray) {
+                    if (s == null) break;
+                    System.out.println(s);
+                }
+                break;
+            default:
+                for (int i = 0; i < inputArray.length; i++) {
+                    if (inputArray[i] == null || inputArray[i].isEmpty()) {
+                        inputArray[i] = inputString;
+                        break;
+                    }
+                }
+                String outMsg = "____________________________________________________________\n" +
+                        "added: " + inputString + "\n" +
+                        "____________________________________________________________\n";
+                System.out.println(outMsg);
+        }
+    }
+
+
     public static void main(String[] args) {
         String outMsg = "____________________________________________________________\n" +
                 " Hello! I'm BasilSeed \n" +
                 " What can I do for you?\n" +
                 "____________________________________________________________\n";
         System.out.println(outMsg);
+        String[] userInputArray = new String[100];
         // Use of scanner to get user input came from https://stackoverflow.com/questions/5287538/how-to-get-the-user-input-in-java
         Scanner reader = new Scanner(System.in);
         System.out.println("Enter a string: ");
-        String userInput = reader.next();
-        for(;!userInput.equals("bye"); userInput = reader.next()){
-            System.out.println("User typed: " + userInput);
+        String userInput = reader.nextLine();
+        for(;!userInput.equals("bye"); userInput = reader.nextLine()){
+            commandParser(userInput, userInputArray);
         }
         System.out.println("""
                     ____________________________________________________________
