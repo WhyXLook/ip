8089d94cd37083e5865b059c0996f039470ab76b
Edmund <l>
2025-09-03 00:55:51 +0800

    Implement changes of taskmanager and inputParser

--- src/main/java/basilseed/BasilSeed.java
+++ src/main/java/basilseed/BasilSeed.java
@@ -1,6 +1,7 @@
 package basilseed;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import basilseed.task.TaskManager;
 import basilseed.ui.UiError;
@@ -10,7 +11,20 @@ import basilseed.ui.UiSuccess;
 
 public class BasilSeed {
 
-    private static void startUp(InputParser inputParser, UiSuccess uiSuccess) {
+    private static void parseAndProcess(String inputString, InputParser inputParser, TaskManager taskManager,
+                                        UiSuccess uiSuccess) {
+        String validString = inputParser.parse(inputString, taskManager.getTaskCount());
+        if (!validString.isEmpty()){
+            String command = inputParser.getCommand(validString);
+            String taskName = inputParser.getTaskName(validString);
+            List<String> argsList = inputParser.getAllArgs(validString);
+            String dateType = inputParser.getDateType(argsList);
+            boolean isMarked = inputParser.isMarked(validString);
+            taskManager.processCommand(command, taskName, argsList, isMarked, dateType);
+        }
+    }
+
+    private static void startUp(InputParser inputParser, TaskManager taskManager, UiSuccess uiSuccess) {
         /*
         Self-explanatory function. Reads storage on startup and initializes
         taskManager's tasks array list with those.
@@ -19,7 +33,7 @@ public class BasilSeed {
         ArrayList<String> taskStrings = storage.read();
         uiSuccess.setSilent(true);
         for (String taskString : taskStrings){
-            inputParser.parse(taskString);
+            parseAndProcess(taskString, inputParser, taskManager, uiSuccess);
         }
         uiSuccess.setSilent(false);
 
@@ -32,12 +46,13 @@ public class BasilSeed {
         UiSuccess uiSuccess = new UiSuccess();
         UiInputOutput uiIo = new UiInputOutput();
         TaskManager taskManager = new TaskManager(uiSuccess);
-        InputParser inputParser = new InputParser(taskManager, uiError);
-        startUp(inputParser, uiSuccess);
+        InputParser inputParser = new InputParser( uiError);
+        startUp(inputParser, taskManager, uiSuccess);
         String userInput = uiIo.getInput();
         for(;!userInput.equals("bye"); userInput = uiIo.getInput()){
-            inputParser.parse(userInput);
+            parseAndProcess(userInput, inputParser, taskManager, uiSuccess);
         }
         uiStandard.displayFarewell();
+        uiIo.close();
     }
 }
